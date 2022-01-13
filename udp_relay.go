822e74fa0c68ad20e259dda9df53c3da21b62dea
lev <lev@shield.asia>
2022-01-13 14:35:13 +0800

    feat[udp]: add udpRelay template code

--- udp_relay.go
+++ udp_relay.go
@@ -1,1 +1,18 @@
 package fdd
+
+import "github.com/rocinan/fdd/poller"
+
+type UDPRelay struct {
+}
+
+func (ur *UDPRelay) AddToLoop(eventLoop *poller.EventLoop) bool {
+	return true
+}
+
+func (ur *UDPRelay) HandleEvent(fd, event int) {}
+
+func (ur *UDPRelay) handleClient() {}
+
+func (ur *UDPRelay) handleServer() {}
+
+func (ur *UDPRelay) Destroy() {}
